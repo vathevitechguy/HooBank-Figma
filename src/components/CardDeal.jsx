780cab69b77344548c3f59af1bb867afef9bd7d4
Victor Aromose <48617966+vathevitechguy@users.noreply.github.com>
2022-10-10 00:32:11 -0400

    Card Deal section completed

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import styles, { layout } from '../style';
+import Button from './Button';
+import { card } from '../assets';
 
 const CardDeal = () => {
-  return <div>CardDeal</div>;
+  return (
+    <section id="cardDeal" className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={`${styles.heading2}`}>
+          Find a better card deal <br className="sm:block hidden" /> in few easy
+          steps.
+        </h2>
+        <p className={`${styles.paragraph}  max-w-[400px] mt-5`}>
+          Arcu tortor, purus in mattis at sed integer faucibus. Aliquet quis
+          aliquet eget mauris tortor.ç Aliquet ultrices ac, ametau.
+        </p>
+
+        <Button styles="mt-10">Get Started</Button>
+      </div>
+      <div className={layout.sectionImg}>
+        <img src={card} alt="network" />
+      </div>
+    </section>
+  );
 };
 
 export default CardDeal;
