d84068d7337b4a36ed4f56aba04768b707cfe209
vathevitechguy <aromosevictor@gmail.com>
2022-10-10 11:50:34 -0400

    Testimonials section completed

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -9,7 +9,7 @@ const Testimonials = () => {
       id="clients"
       className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}
     >
-      <div />
+      <div className="absolute -right-1/2 z-0 w-[60%] h-[60%] rounded-full blue__gradient" />
 
       <div
         className={`w-full flex items-center justify-between md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]`}
