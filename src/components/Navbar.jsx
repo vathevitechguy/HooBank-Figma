a96f51451aeee2036dca8034255729ee889a67a2
Victor Aromose <48617966+vathevitechguy@users.noreply.github.com>
2022-10-09 20:43:31 -0400

    Adding elements to Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
 
+import { close, logo, menu } from '../assets';
+import { navLinks } from '../constants';
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="w-full flex py6 justify-between items-center navbar">
+      <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
+
+      <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+        {navLinks.map((nav, index) => {
+          <li
+            key={nav.id}
+            className={`font-poppins font-normal cursor-pointer text-[16px] text-black`}
+          >
+            <a href={`#${nav.id}`}>{nav.title}</a>
+          </li>;
+        })}
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
